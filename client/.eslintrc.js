bfbb0f06f1e9e33d1e1dd3bb477c47becf774220
Imad Shatali <amoodaa@gmail.com>
2021-10-21 10:59:59 +0300

    chore: create-react-app and eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
