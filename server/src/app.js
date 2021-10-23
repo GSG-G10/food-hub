fa71b5793828bec2fa0034a6ba37e83f877d2d77
NourAdawi <nouradwi12@gmail.com>
2021-10-24 00:51:20 +0300

    chore: set up server

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+require('env2')('../.env');
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const { join } = require('path');
+const router = require('./router');
+
+const app = express();
+
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 8080);
+
+app.use('/api/v1', router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', '..', 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = app;
