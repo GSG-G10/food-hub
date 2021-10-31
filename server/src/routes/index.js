541445f2e63a458be23bfd8809f86a356f1be96e
NourAdawi <nouradwi12@gmail.com>
2021-10-31 20:13:01 +0200

    fix: router file

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,13 +3,11 @@ const mealsRouter = require('./meals');
 const categoryRouter = require('./categories');
 const restaurantRouter = require('./restaurants');
 const promoCodeRouter = require('./promocode');
-const redeemRouter = require('./redeemRouter');
 const { auth } = require('../controllers');
 const { decodeToken } = require('../middleware/privateRoute');
 
 router.get('/auth', decodeToken, auth);
-router.post('/promocode', decodeToken, promoCodeRouter);
-router.post('/redeem', decodeToken, redeemRouter);
+router.post('/promo', decodeToken, promoCodeRouter);
 router.use('/meals', mealsRouter);
 router.use('/restaurant', restaurantRouter);
 router.use('/category', categoryRouter);
