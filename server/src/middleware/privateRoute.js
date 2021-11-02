3aa35f92086dff4b4c332a75a9e844f2455e83e4
NourAdawi <nouradwi12@gmail.com>
2021-11-02 23:30:27 +0200

    fix: response msg

--- server/src/middleware/privateRoute.js
+++ server/src/middleware/privateRoute.js
@@ -1,6 +1,6 @@
 const admin = require('../config/firebase');
 
-exports.decodeToken = async (req, res, next) => {
+exports.isAuth = async (req, res, next) => {
   const { authorization } = req.headers;
 
   try {
@@ -11,8 +11,8 @@ exports.decodeToken = async (req, res, next) => {
     if (decodevalue) return next();
     return res
       .status(400)
-      .json({ success: false, msg: 'You are not authorized!' });
+      .json({ msg: 'You are not authorized!' });
   } catch (err) {
-    return res.json({ success: false, msg: err });
-  }
+    next(err)
+    }
 };
