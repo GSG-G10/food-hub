b344441cc70cfc088b6ceb174f2da4ccc87b5ee9
NourAdawi <nouradwi12@gmail.com>
2021-11-08 19:25:15 +0200

    fix: add snackprovider

--- client/src/App.js
+++ client/src/App.js
@@ -66,32 +66,34 @@ export const App = () => (
     <CssBaseline />
     <Router>
       <AuthProvider>
-        <CartProvider>
-          <ThemeProvider theme={theme}>
-            <Header />
-            <Switch>
-              <Route exact path="/" />
-              <Route exact path="/register">
-                <Register />
-              </Route>
-              <Route exact path="/restaurants">
-                <Restaurants />
-              </Route>
-              <Route exact path="/categories">
-                <Categories />
-              </Route>
-              <Route exact path="/category/:id">
-                <Category />
-              </Route>
-              <Route exact path="/restaurants/:id">
-                <Restaurant />
-              </Route>
-              <Route path="/about-us" />
-              <Route path="/login" />
-            </Switch>
-            <Footer />
-          </ThemeProvider>
-        </CartProvider>
+        <SnackbarProvider>
+          <CartProvider>
+            <ThemeProvider theme={theme}>
+              <Header />
+              <Switch>
+                <Route exact path="/" />
+                <Route exact path="/register">
+                  <Register />
+                </Route>
+                <Route exact path="/restaurants">
+                  <Restaurants />
+                </Route>
+                <Route exact path="/categories">
+                  <Categories />
+                </Route>
+                <Route exact path="/category/:id">
+                  <Category />
+                </Route>
+                <Route exact path="/restaurants/:id">
+                  <Restaurant />
+                </Route>
+                <Route path="/about-us" />
+                <Route path="/login" />
+              </Switch>
+              <Footer />
+            </ThemeProvider>
+          </CartProvider>
+        </SnackbarProvider>
       </AuthProvider>
     </Router>
   </>
